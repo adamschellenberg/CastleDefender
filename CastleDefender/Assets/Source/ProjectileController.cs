2fe45dce0f12b238c3d2afc84b82b7e879070b42
Jacob Schellenberg <=>
2018-09-01 13:08:34 -0700

    Created Character/Projectile controllers.

---
+++ CastleDefender/Assets/Source/ProjectileController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 1;
+    [SerializeField] private float _damage = 1;
+
+    private void Start()
+    {
+        Destroy(this.gameObject, 5);
+    }
+
+    private void Update()
+    {
+        this.transform.Translate(Vector3.right * Time.deltaTime * _moveSpeed);
+
+        //this.transform.position = Vector2.MoveTowards(this.transform.position, _targetPosition, _moveSpeed * Time.deltaTime);
+
+        //if(Vector2.Distance(this.transform.position, _targetPosition) <= 0.1f)
+        //{
+        //    Destroy(this.gameObject);
+        //}
+    }
+}

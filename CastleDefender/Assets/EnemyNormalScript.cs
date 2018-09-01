aac7d2198d21e42c01e9131facf799a945a61710
Adam Schellenberg <adamschellenberg93@gmail.com>
2018-09-01 09:10:48 -0700

    Move left to right
    Added left-to-right movement

---
+++ CastleDefender/Assets/EnemyNormalScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyNormalScript : MonoBehaviour {
+
+	[SerializeField]
+	float moveSpeed;
+
+	[SerializeField]
+	Collider2D enemyCollider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate(new Vector2(moveSpeed * Time.deltaTime, 0));
+		
+	}
+}

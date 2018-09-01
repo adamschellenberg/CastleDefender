f8bb12b0be73606fceb41b77ab22995feeaca564
Jacob Schellenberg <=>
2018-09-01 10:05:50 -0700

    Wired up Start/End menus. Created PlayerPrefsManager for score keeping. Removed PauseMenu.

---
+++ CastleDefender/Assets/Source/UI/EndMenuController.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndMenuController : MonoBehaviour 
+{
+    [SerializeField] private Button _playAgainButton;
+    [SerializeField] private Button _backToMenuButton;
+    [SerializeField] private TMP_Text _highScoreText;
+    [SerializeField] private TMP_Text _currentScoreText;
+
+    private void Awake()
+    {
+        _playAgainButton.onClick.AddListener(OnPlayAgainButtonClicked);
+        _backToMenuButton.onClick.AddListener(OnBackToMenuButtonClicked);
+    }
+
+    public void OnPlayAgainButtonClicked()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OnBackToMenuButtonClicked()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void SetHighScoreText()
+    {
+        int highScore = PlayerPrefsManager.GetHighScore();
+        _highScoreText.text = highScore.ToString("00000");
+    }
+
+    public void SetCurrentScore()
+    {
+        int currentScore = PlayerPrefsManager.GetCurrentScore();
+        _currentScoreText.text = currentScore.ToString("00000");
+    }
+}
---
+++ CastleDefender/Assets/Source/UI/StartMenuController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenuController : MonoBehaviour 
+{
+    [SerializeField] private Button _playButton;
+
+    private void Awake()
+    {
+        _playButton.onClick.AddListener(OnPlayButtonClicked);
+    }
+
+    public void OnPlayButtonClicked()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}

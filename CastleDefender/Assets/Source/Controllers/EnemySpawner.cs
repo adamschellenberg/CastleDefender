72aad291c69152450b74609f6abaf860a4b93aee
Adam Schellenberg <adamschellenberg93@gmail.com>
2018-09-01 17:39:15 -0700

    difficult increases with higher score

--- CastleDefender/Assets/Source/Controllers/EnemySpawner.cs
+++ CastleDefender/Assets/Source/Controllers/EnemySpawner.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private float _spawnPositionX = -11;
+    [SerializeField] private float _spawnPositionX = -13;
     [SerializeField] private Vector2 _spawnPositionYRange = new Vector2(-4f, 4.5f);
 
     [SerializeField] private EnemyController _normalEnemy;
-    [SerializeField] private float normalEnemySpawnRate;
+    [SerializeField] private float normalEnemySpawnRateDefault;
+	[SerializeField] private float currentNormalEnemySpawnRate;
     private float _normalEnemySpawnCooldown;
 
     [SerializeField] private EnemyController _fastEnemy;
-    [SerializeField] private float fastEnemySpawnRate;
+    [SerializeField] private float fastEnemySpawnRateDefault;
+	[SerializeField] private float currentFastEnemySpawnRate;
     private float _fastEnemySpawnCooldown;
 
     [SerializeField] private EnemyController _heavyEnemy;
-    [SerializeField] private float heavyEnemySpawnRate;
+    [SerializeField] private float heavyEnemySpawnRateDefault;
+	[SerializeField] private float currentHeavyEnemySpawnRate;
     private float _heavyEnemySpawnCooldown;
 
     private List<EnemyController> _enemyPool = new List<EnemyController>();
@@ -27,9 +30,13 @@ public class EnemySpawner : MonoBehaviour
 
     private void Start()
     {
-        _normalEnemySpawnCooldown = normalEnemySpawnRate;
-        _fastEnemySpawnCooldown = fastEnemySpawnRate;
-        _heavyEnemySpawnCooldown = heavyEnemySpawnRate;
+		currentNormalEnemySpawnRate = normalEnemySpawnRateDefault;
+		currentFastEnemySpawnRate = fastEnemySpawnRateDefault;
+		currentHeavyEnemySpawnRate = heavyEnemySpawnRateDefault;
+
+        _normalEnemySpawnCooldown = currentNormalEnemySpawnRate;
+        _fastEnemySpawnCooldown = currentFastEnemySpawnRate;
+        _heavyEnemySpawnCooldown = currentHeavyEnemySpawnRate;
 
         _castleController = GameObject.FindObjectOfType<CastleController>();
 
@@ -41,24 +48,27 @@ public class EnemySpawner : MonoBehaviour
         if (_normalEnemySpawnCooldown <= 0)
         {
             SpawnEnemy(_normalEnemy, EnemyType.Normal);
-            _normalEnemySpawnCooldown = normalEnemySpawnRate;
+            _normalEnemySpawnCooldown = currentNormalEnemySpawnRate;
         }
 
         if (_fastEnemySpawnCooldown <= 0)
         {
             SpawnEnemy(_fastEnemy, EnemyType.Fast);
-            _fastEnemySpawnCooldown = fastEnemySpawnRate;
+            _fastEnemySpawnCooldown = currentFastEnemySpawnRate;
         }
 
         if (_heavyEnemySpawnCooldown <= 0)
         {
             SpawnEnemy(_heavyEnemy, EnemyType.Heavy);
-            _heavyEnemySpawnCooldown = heavyEnemySpawnRate;
+            _heavyEnemySpawnCooldown = currentHeavyEnemySpawnRate;
         }
 
         _normalEnemySpawnCooldown -= Time.deltaTime;
         _fastEnemySpawnCooldown -= Time.deltaTime;
         _heavyEnemySpawnCooldown -= Time.deltaTime;
+
+		IncreaseDifficulty ();
+
     }
 
     private void SpawnEnemy(EnemyController enemyPrefab, EnemyType enemyType)
@@ -79,4 +89,14 @@ public class EnemySpawner : MonoBehaviour
 
         pooledEnemyController.Activate(position);
     }
+
+	private void IncreaseDifficulty() {
+
+		float currentScore = (float) PlayerPrefsManager.GetCurrentScore ();
+
+		currentNormalEnemySpawnRate = normalEnemySpawnRateDefault - (currentScore / 250);
+		currentFastEnemySpawnRate = fastEnemySpawnRateDefault - (currentScore / 500);
+		currentHeavyEnemySpawnRate = heavyEnemySpawnRateDefault - (currentScore / 750);
+
+	}
 }
